b63cd2c059620134a43a81a696d2a7d51fab2b65
José Melo <josecoemelo@gmail.com>
2023-04-19 23:11:30 +0100

    update source URL

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const axios = require('axios');
 const getSingleStockInfo = stock => new Promise((resolve, reject) => {
     if (!stock) { return reject(Error('Stock symbol required')); }
     if (typeof stock !== 'string') { return reject(Error(`Invalid argument type. Required: string. Found: ${typeof stock}`)); }
-    
-    const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${stock}`;    
-    return axios.get(url).then((res) => {
-        const { data } = res;
-        if (!data || !data.quoteResponse || !data.quoteResponse.result || !data.quoteResponse.result.length) {
-            return resolve(new Error(`Error retrieving info for symbol ${stock}`));
-        }
-        return resolve(data.quoteResponse.result[0]);
-    }).catch(err => reject(err));
+
+    return axios.get(`https://query2.finance.yahoo.com/v10/finance/quoteSummary/${stock}?formatted=false&modules=price%2CfinancialData%2CsummaryDetail`)          
+        .then(res => {
+            const { data } = res;
+            if (!data || !data.quoteSummary.result || !data.quoteSummary.result.length) {
+                return reject(new Error(`Error retrieving info for symbol ${stock}`));
+            }
+            return resolve(data.quoteSummary.result[0]);
+        })
+        .catch(err => reject(err));
 });
 
 const getStocksInfo = stockList => new Promise((resolve, reject) => {
